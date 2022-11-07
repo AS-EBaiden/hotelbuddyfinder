e43c8c1474615a6cdd1adbc5214114036531c6a1
ebisLab <ebaiden@techie.com>
2022-11-07 00:13:55 -0500

    form submit success message

--- src/views/Matched.js
+++ src/views/Matched.js
@@ -1,33 +1,42 @@
 import React, { useState } from "react";
-
+import { doc, setDoc } from "firebase/firestore";
+import { db, storage } from "../firebase";
 export default function Matched({ personInfo, setPersonInfo }) {
   const [initialValues, setInitialValues] = useState({
     username: "",
     username2: "",
   });
 
-  const [inputValue, setInputValue] = useState("");
-  const [contactData, setContacData] = useState([]);
+  const [successSubmit, setSuccessSubmit] = useState(false);
 
   const submitMatch = (e) => {
     e.preventDefault();
 
-    let firstInput = personInfo.map((obj) => {
+    let firstInput = personInfo.map(async (obj) => {
       if (obj.username === initialValues.username) {
         if (obj.isMatched === "true") return;
+
+        await setDoc(doc(db, "users", obj.id), {
+          ...obj,
+          isMatched: "true",
+        });
         return { ...obj, isMatched: "true" };
       }
       if (initialValues.username2 !== "") {
         if (obj.username === initialValues.username2) {
           if (obj.isMatched === "true") return;
+          await setDoc(doc(db, "users", obj.id), {
+            ...obj,
+            isMatched: "maybe",
+          });
           return { ...obj, isMatched: "maybe" };
         }
-        // return { ...obj, isMatched: "maybe" };
       }
       return obj;
     });
 
     setPersonInfo(firstInput);
+    setSuccessSubmit(true);
   };
 
   const changeHandler = (e) => {
@@ -55,29 +64,36 @@ export default function Matched({ personInfo, setPersonInfo }) {
           alignItems: "center",
         }}
       >
-        <form onSubmit={submitMatch} className="match-form">
-          <div style={{ padding: "2%" }}>
-            <label>Your Username</label>
-            <input
-              placeholder="your username"
-              className="matched-inputs"
-              onChange={changeHandler}
-              name="username"
-            />
+        {successSubmit ? (
+          <div style={{ padding: "0 0 20%" }}>
+            <h3 style={{ fontSize: "2.5rem", color: "#77bd7d" }}>Success!!</h3>
+            <h3>Enjoy AfroTech 2022!</h3>
           </div>
-          <div style={{ padding: "2%" }}>
-            <label>Your roomate username</label>
-            <input
-              placeholder="roomate's username"
-              className="matched-inputs"
-              onChange={changeHandler}
-              name="username2"
-            />
-          </div>
-          <div>
-            <button className="match-btn">I matched</button>
-          </div>
-        </form>
+        ) : (
+          <form onSubmit={submitMatch} className="match-form">
+            <div style={{ padding: "2%" }}>
+              <label>Your Username</label>
+              <input
+                placeholder="your username"
+                className="matched-inputs"
+                onChange={changeHandler}
+                name="username"
+              />
+            </div>
+            <div style={{ padding: "2%" }}>
+              <label>Your roomate username</label>
+              <input
+                placeholder="roomate's username"
+                className="matched-inputs"
+                onChange={changeHandler}
+                name="username2"
+              />
+            </div>
+            <div>
+              <button className="match-btn">I matched</button>
+            </div>
+          </form>
+        )}
       </div>
     </div>
   );
